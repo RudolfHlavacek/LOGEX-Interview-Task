0516f7f59db754a0b8dd1f48b9268ff726085cbf
Ruda <rudolf.hlavacek@gmail.com>
2020-11-14 17:08:03 +0100

    Created classes Record - eval of string and parsing; Employees - stores, manages and returns formatted records.

--- LOGEX-Interview-Task/Program.cs
+++ LOGEX-Interview-Task/Program.cs
@@ -10,6 +10,15 @@ namespace LOGEX_Interview_Task
     {
         static void Main(string[] args)
         {
+            string[] input_strings = { "020_Jan.Krutý", "21_Petr.Malý", "021_Pavel.Novák", "514_Karel.Ospalý" };
+            string[] expected_strings = { "021 2x: Malý, Novák", "020 1x: Krutý", "514 1x: Ospalý" };
+
+            foreach (string s in input_strings)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ LOGEX-Interview-Task/Record.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGEX_Interview_Task
+{
+    class Record
+    {
+        private string redex;
+        private string record;
+
+        public Record(string redex)
+        {
+            this.redex = redex;
+        }
+
+        public Record(string redex, string record)
+        {
+            this.redex = redex;
+            this.record = record;
+        }
+
+        public void ChangeRecord(string new_record)
+        {
+            this.record = new_record;
+        }
+
+        public bool IsValid()
+        {
+
+        }
+
+        public int GetNumber()
+        {
+
+        }
+
+        public string GetLastName()
+        {
+
+        }
+    }
+}
