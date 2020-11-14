0d2f288bb31da62927f9a0bd7a433743cd285188
Ruda <rudolf.hlavacek@gmail.com>
2020-11-14 13:18:01 +0100

    Creates MS Visual Studio project.

---
+++ LOGEX-Interview-Task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGEX_Interview_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
